ebc7ceedf8b9fb930b5ba5ae7fd0c42bfbc04d14
Manfred Touron <m@42.am>
2016-07-16 22:29:45 +0200

    Use memoize

--- pkg/soundcloud/soundcloud.go
+++ pkg/soundcloud/soundcloud.go
@@ -1,8 +1,10 @@
 package calcsoundcloud
 
 import (
+	"net/url"
 	"strings"
 
+	"github.com/shazow/memoizer"
 	"github.com/yanatan16/golang-soundcloud/soundcloud"
 )
 
@@ -10,6 +12,7 @@ type CALCSoundcloud struct {
 	client   *soundcloud.Api
 	clientID string
 	userID   uint64
+	cache    memoizer.Memoize
 }
 
 func New(clientID string, userID uint64) CALCSoundcloud {
@@ -19,6 +22,9 @@ func New(clientID string, userID uint64) CALCSoundcloud {
 		},
 		clientID: clientID,
 		userID:   userID,
+		cache: memoizer.Memoize{
+			Cache: memoizer.NewMemoryCache(),
+		},
 	}
 }
 
@@ -27,21 +33,32 @@ func (s *CALCSoundcloud) EscapeString(input string) string {
 }
 
 func (s *CALCSoundcloud) Me() (*soundcloud.User, error) {
-	return s.client.User(s.userID).Get(nil)
+	user, err := s.cache.Call(s.client.User(s.userID).Get, url.Values{})
+	return user.(*soundcloud.User), err
 }
 
 func (s *CALCSoundcloud) Playlists() ([]*soundcloud.Playlist, error) {
-	return s.client.User(s.userID).Playlists(nil)
+	playlists, err := s.cache.Call(s.client.User(s.userID).Playlists, url.Values{})
+	return playlists.([]*soundcloud.Playlist), err
 }
 
 func (s *CALCSoundcloud) Playlist(playlistID uint64) (*soundcloud.Playlist, error) {
-	return s.client.Playlist(playlistID).Get(nil)
+	inline := func(playlistID uint64) (*soundcloud.Playlist, error) {
+		return s.client.Playlist(playlistID).Get(nil)
+	}
+	playlist, err := s.cache.Call(inline, playlistID)
+	return playlist.(*soundcloud.Playlist), err
 }
 
 func (s *CALCSoundcloud) Track(trackID uint64) (*soundcloud.Track, error) {
-	return s.client.Track(trackID).Get(nil)
+	inline := func(trackID uint64) (*soundcloud.Track, error) {
+		return s.client.Track(trackID).Get(nil)
+	}
+	track, err := s.cache.Call(inline, trackID)
+	return track.(*soundcloud.Track), err
 }
 
 func (s *CALCSoundcloud) Tracks() ([]*soundcloud.Track, error) {
-	return s.client.User(s.userID).Tracks(nil)
+	tracks, err := s.cache.Call(s.client.User(s.userID).Tracks, url.Values{})
+	return tracks.([]*soundcloud.Track), err
 }
