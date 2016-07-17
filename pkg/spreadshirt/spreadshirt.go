257d82f175c4e7babb1ae4e87341f49f0dd1360f
Manfred Touron <m@42.am>
2016-07-17 22:22:30 +0200

    Add new t-shirts

--- pkg/spreadshirt/spreadshirt.go
+++ pkg/spreadshirt/spreadshirt.go
@@ -17,10 +17,72 @@ var products = []Product{
 	{
 		Title:   "Cours de sport",
 		CompoID: 124452996,
-	},
-	{
+	}, {
 		Title:   "Dino simple",
 		CompoID: 124453182,
+	}, {
+		Title:   "Dino rainbow",
+		CompoID: 124452969,
+	}, {
+		Title:   "Calc degrade",
+		CompoID: 124452956,
+	}, {
+		Title:   "Calc pixel",
+		CompoID: 124452945,
+	}, {
+		Title:   "J'veux faire des trucs",
+		CompoID: 124452911,
+	}, {
+		Title:   "Gars cool approved",
+		CompoID: 124452840,
+	}, {
+		Title:   "Gars cool fait du rap",
+		CompoID: 124403201,
+	}, {
+		Title:   "Un bon plan",
+		CompoID: 124403188,
+	}, {
+		Title:   "Calc white-on-black",
+		CompoID: 124399349,
+	}, {
+		Title:   "Trop de la balle",
+		CompoID: 124399356,
+	}, {
+		Title:   "DJ Jean-Luc Puissant",
+		CompoID: 124399370,
+	}, {
+		Title:   "C'est la fiesta, wouhouuu",
+		CompoID: 124399385,
+	}, {
+		Title:   "A la campagne, il y a un banc",
+		CompoID: 124399407,
+	}, {
+		Title:   "50% bacon, 50% biere, 50% camembert",
+		CompoID: 124399002,
+	}, {
+		Title:   "Des trucs",
+		CompoID: 124399027,
+	}, {
+		Title:   "Pochette camembert",
+		CompoID: 124399084,
+	}, {
+		Title:   "Une gomme",
+		CompoID: 124399103,
+	}, {
+		Title:   "Une vraie gomme",
+		CompoID: 124399138,
+	}, {
+		Title:   "TPYO",
+		CompoID: 133595868,
+	}, {
+		Title:   "Millipede",
+		CompoID: 133595825,
+	}, {
+		Title:   "Sapin",
+		CompoID: 133595722,
+	}, {
+		Title:   ":ok_hand:",
+		CompoID: 133595728,
 	},
 }
 
