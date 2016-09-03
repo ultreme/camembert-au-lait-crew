b3e68be425f4bf2bfe69ed1e4fba3120c85f8aa5
Manfred Touron <m@42.am>
2016-09-03 18:57:29 +0200

    Testing if number is prime

--- pkg/numberinfo/numberinfo.go
+++ pkg/numberinfo/numberinfo.go
@@ -1,15 +1,22 @@
 package calcnumberinfo
 
+import "github.com/moul/numberinfo"
+
 type CALCNumberInfo struct {
-	number float64
+	input  float64
+	number *numberinfo.Float64Number
 }
 
 func New(number float64) *CALCNumberInfo {
-	return &CALCNumberInfo{number: number}
+	return &CALCNumberInfo{
+		input:  number,
+		number: numberinfo.Float64(number),
+	}
 }
 
 func (n *CALCNumberInfo) All() map[string]interface{} {
 	ret := map[string]interface{}{}
-	ret["number"] = n.number
+	ret["number"] = n.input
+	ret["is-prime"] = n.number.IsPrime()
 	return ret
 }
